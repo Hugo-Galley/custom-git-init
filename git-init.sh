98faa1a61a51b503d57882c23c006570f88678a9
Freaks <galleyhugo@icloud.com>
2025-09-05 16:54:00 +0200

    features(ajout) : ajout du fichier bash + readme

---
+++ git-init.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+git init
+touch .gitignore
+
+# Récuperation des templates
+git clone https://github.com/Hugo-Galley/custom-git-init
+cat custom-git-init/template-README.MD > README.MD || echo "Erreur lors de la création du fichier README"
+cat custom-git-init/template-LICENCE.MD > LICENCE.MD || echo "Erreur lors de la création du fichier de Licence"
+
+
+# Insertion des variables dans les fichiers ciblé
+folder_name=$(basename "$PWD")
+year=$(date +%Y)
+# Création de la liste des guidelines à suivres
+guidelines_list=$(
+    for file in custom-git-init/guidelines/*; do
+        name="$(basename "$file")"
+        echo "- [${name%.*}](https://github.com/Hugo-Galley/custom-git-init/tree/main/guidelines/$name)"
+    done
+)
+export folder_name year guidelines_list
+envsubst < README.MD > README.tmp && mv README.tmp README.MD
+envsubst < LICENCE.MD > LICENCE.tmp && mv LICENCE.tmp LICENCE.MD
+
+# ajout des fichier au suivi git
+git add README.MD LICENCE.MD
+
+# suppresion du dossier de template
+rm -rf custom-git-init
